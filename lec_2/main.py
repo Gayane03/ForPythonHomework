1394768010b7183dc75e91cbb47d110eda5b3886
Gayane Tsaturyan <gayane.tsaturyan.2020@gmail.ru>
2023-10-29 22:28:02 +0400

    1-8 lec

---
+++ lec_2/main.py
@@ -0,0 +1,29 @@
+i: int =7;
+print(i);
+
+f: float = 9.55;
+print(f);
+
+s: str ="Hello";
+print(s);
+
+'''
+input=input();
+print(input);
+
+try:
+    input=int(input);
+    print("Integer number : ",input); 
+except:
+    try:
+        input=float(input);
+        print("Float number : ",input);
+    except:
+        print("Invalid input. Please enter a valid number.");
+'''
+
+print(i+f);
+print(i-f);
+print(i*f);
+print(i/f);
+print(f%i);
